2c34337a254297464ac1aeca49ef971601dc6a4b
shizuku86 <shizuku86710@gmail.com>
2025-11-15 01:54:06 +0900

    prettierr formated & build

---
+++ BP/scripts/Kairo/constants/scriptevent.js
@@ -0,0 +1,18 @@
+export const SCRIPT_EVENT_ID_PREFIX = {
+    KAIRO: "kairo",
+};
+export const SCRIPT_EVENT_IDS = {
+    BEHAVIOR_REGISTRATION_REQUEST: "kairo:registrationRequest",
+    BEHAVIOR_REGISTRATION_RESPONSE: "kairo:registrationResponse",
+    BEHAVIOR_INITIALIZE_REQUEST: "kairo:initializeRequest",
+    UNSUBSCRIBE_INITIALIZE: "kairo:unsubscribeInitialize",
+    REQUEST_RESEED_SESSION_ID: "kairo:reseedSessionId",
+    SHOW_ADDON_LIST: "kairo:showAddonList",
+};
+export const SCRIPT_EVENT_MESSAGES = {
+    ACTIVATE_REQUEST: "activate request",
+    DEACTIVATE_REQUEST: "deactivate request",
+};
+export const SCRIPT_EVENT_COMMAND_IDS = {
+    DATA_LOADED: "data_loaded",
+};
--- scripts/Kairo/addons/router/init/AddonInitializeResponse.ts
+++ scripts/Kairo/addons/router/init/AddonInitializeResponse.ts
@@ -7,28 +7,32 @@ import { SCRIPT_EVENT_IDS } from "../../../constants/scriptevent";
 /**
  * アドオンの properties を参照して、ルーターに応答するためのクラス
  * propertiesの必要な部分を抜粋して、JSON.stringifyで送信します
- * 
+ *
  * A class that responds to the router by referencing the addon's properties
  * Extracts the necessary parts of the properties and sends them using JSON.stringify
  */
 export class AddonInitializeResponse {
-    private constructor(private readonly addonInitializer: AddonInitializer) {}
+  private constructor(private readonly addonInitializer: AddonInitializer) {}
 
-    public static create(addonInitializer: AddonInitializer): AddonInitializeResponse {
-        return new AddonInitializeResponse(addonInitializer);
-    }
+  public static create(
+    addonInitializer: AddonInitializer,
+  ): AddonInitializeResponse {
+    return new AddonInitializeResponse(addonInitializer);
+  }
 
-    /**
-     * scoreboard を使って登録用の識別番号も送信しておく
-     * Also send the registration ID using the scoreboard
-     */
-    public sendResponse(addonProperty: AddonProperty): void {
-        system.sendScriptEvent(
-            SCRIPT_EVENT_IDS.BEHAVIOR_REGISTRATION_RESPONSE, 
-            JSON.stringify([
-                addonProperty,
-                world.scoreboard.getObjective(SCOREBOARD_NAMES.ADDON_COUNTER)?.getScore(SCOREBOARD_NAMES.ADDON_COUNTER) ?? 0
-            ])
-        );
-    }
+  /**
+   * scoreboard を使って登録用の識別番号も送信しておく
+   * Also send the registration ID using the scoreboard
+   */
+  public sendResponse(addonProperty: AddonProperty): void {
+    system.sendScriptEvent(
+      SCRIPT_EVENT_IDS.BEHAVIOR_REGISTRATION_RESPONSE,
+      JSON.stringify([
+        addonProperty,
+        world.scoreboard
+          .getObjective(SCOREBOARD_NAMES.ADDON_COUNTER)
+          ?.getScore(SCOREBOARD_NAMES.ADDON_COUNTER) ?? 0,
+      ]),
+    );
+  }
 }
