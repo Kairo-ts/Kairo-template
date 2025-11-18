a8041d6ab36eeb220d3252f27ad037c480c32cd0
shizuku86 <shizuku86710@gmail.com>
2025-11-18 10:49:28 +0900

    fixed KairoUtils

--- scripts/index.ts
+++ scripts/index.ts
@@ -24,9 +24,9 @@ Kairo.onDeactivate = () => {
 Kairo.onScriptEvent = (data: KairoCommand) => {
     /**
      * ここにはアドオンが scriptEvent を受け取った際の処理を書く
-     * 利用できるプロパティは { message } のみ
+     * 利用できるプロパティは { data: KairoCommand } のみ
      * Write the handler logic for when the addon receives a scriptEvent
-     * The only available property is { message }
+     * The only available property is { data: KairoCommand }
      */
 };
 
